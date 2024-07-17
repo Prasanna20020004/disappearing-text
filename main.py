9b9ca4a1ec3ee359c762618279c19b9455c149f4
Prasanna2002004 <kotkarprasanna96@gmail.com>
2024-07-17 08:54:38 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+
+
+def set_timer():
+    previous_text = text.get()
+    window.after(5000, timer, previous_text)
+
+
+def timer(previous_text):
+    current_text = text.get()
+
+    if len(previous_text) == len(current_text):
+        text.delete(0, tk.END)
+
+    set_timer()
+
+
+window = tk.Tk()
+window.title("Disappearing Text")
+window.minsize(width=500, height=200)
+
+label = tk.Label(text="Type here", font=("Arial", 24, "bold"))
+label.pack()
+
+text = tk.Entry(width=100)
+text.focus()
+text.pack()
+set_timer()
+
+window.mainloop()
